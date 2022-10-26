63a773049a92d3c2d4840f7cfa7e79a6c92a5f20
jonquiiillll <71707079+jonquiiillll@users.noreply.github.com>
2022-10-26 22:40:01 +0300

    Init commit

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,42 @@
 @prototypes_data = [
   {
     name: 'First module / First prototype'
+  },
+  {
+    name: 'First module / Second prototype'
+  },
+  {
+    name: 'First module / Third prototype'
+  },
+  {
+    name: 'Effects'
+  },
+  {
+    name: 'Firts Melody, Tone.js'
+  },
+  {
+    name: 'Different types of synth'
+  },
+  {
+    name: 'Interface // Tone.js'
+  },
+  {
+    name: 'Synth 3rd melody // Tone.js'
+  },
+  {
+    name: 'Tone.js wuth UI and Two Synths'
+  },
+  {
+    name: 'Tone.js with UI // Composition'
+  },
+  {
+    name: 'Tone.js with UI // Compos 2.0'
+  },
+  {
+    name: 'Tone.js with UI // Samples'
+  },
+  {
+    name: 'Tone.js // Final Synth'
   }
 ]
 
